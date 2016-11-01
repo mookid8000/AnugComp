0c4a1fdc9bd27726d36f17f7b4091b4664f6811a
Mogens Heller Grabe <mookid8000@gmail.com>
2016-11-01 22:21:09 +0100

    bam!

---
+++ AnugComp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Tababular;
+
+namespace AnugComp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var formatter = new TableFormatter(new Hints { CollapseVerticallyWhenSingleLine = true, MaxTableWidth = 120 });
+
+            var objects = File.ReadAllText("Serverless_computing_-_the_most_fluffy_cloud.xls")
+                .Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .Select(line => line.Split('\t'))
+                .Select(cells => new
+                {
+                    Name = cells[0],
+                    Title = cells[2],
+                    Host = cells[3],
+                    Profile = cells[8]
+                });
+
+            Console.WriteLine(formatter.FormatObjects(objects));
+        }
+    }
+}
---
+++ AnugComp/RandomExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnugComp
+{
+    public static class RandomExtensions
+    {
+        public static IEnumerable<T> InRandomOrder<T>(this IEnumerable<T> elements)
+        {
+            var list = elements.ToList();
+            var random = new Random(DateTime.Now.GetHashCode());
+            var iterations = list.Count;
+
+            Action swapElements = () =>
+            {
+                var index1 = random.Next(list.Count);
+                var index2 = random.Next(list.Count);
+                var elem1 = list[index1];
+                var elem2 = list[index2];
+                list[index2] = elem1;
+                list[index1] = elem2;
+            };
+
+            while (iterations-- > 0)
+            {
+                swapElements();
+            }
+
+            return list;
+        }
+    }
+}
